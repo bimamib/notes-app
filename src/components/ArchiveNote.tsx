dce1dd2ebfd83e23109d5dbc66af1f955293d9f2
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-13 23:49:02 +0700

    Feat: Archive Note

---
+++ src/components/ArchiveNote.tsx
@@ -0,0 +1,21 @@
+import NoteList from "./NoteList";
+import { NoteListProps } from "../utils/types";
+
+const ArchiveNote = ({
+  notes,
+  deleteNote,
+  toggleArchiveNote,
+}: NoteListProps) => {
+  return (
+    <div>
+      <h1 className="mb-5 text-xl font-bold lg:text-2xl">Archived Notes</h1>
+      <NoteList
+        notes={notes}
+        deleteNote={deleteNote}
+        toggleArchiveNote={toggleArchiveNote}
+      />
+    </div>
+  );
+};
+
+export default ArchiveNote;
