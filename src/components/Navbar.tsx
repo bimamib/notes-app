446267d6330fe66856a70ced3b6634e02effcf2b
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-13 20:55:51 +0700

    Feat: Component Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { NavbarProps } from "../utils/types";
+
+const Navbar = ({ onSearch }: NavbarProps) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <header className="bg-slate-50 p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold lg:text-4xl">Notes</h1>
+      <input
+        className="navbar-search-input p-2 border border-gray-200 focus:border-blue-500 focus:outline-blue-500 rounded-md w-3/5 mr-2 text-sm"
+        type="text"
+        placeholder="Search notes..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <button
+        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+        type="button"
+        onClick={handleSearchClick}
+      >
+        Search
+      </button>
+    </header>
+  );
+};
+
+export default Navbar;
