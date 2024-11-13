c7569e42633fbccdae97633aab56a1495d68b0cf
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-14 01:10:37 +0700

    Nambahin grid cols

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -7,7 +7,7 @@ const NoteList = ({ notes, deleteNote, toggleArchiveNote }: NoteListProps) => {
   }
 
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {notes.map((note) => (
         <NoteItem
           key={note.id}
