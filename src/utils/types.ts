a8ba1191017daf51cfcc1a8987628aecec718765
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-13 23:49:54 +0700

    Add: types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,27 @@
 export type NavbarProps = {
   onSearch: (keyword: string) => void;
 };
+
+export type Note = {
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+  archived: boolean;
+};
+
+export type AddNoteProps = {
+  addNote: (title: string, body: string) => void;
+};
+
+export type NoteListProps = {
+  notes: Note[];
+  deleteNote: (id: number) => void;
+  toggleArchiveNote: (id: number) => void;
+};
+
+export type NoteItemProps = {
+  note: Note;
+  deleteNote: (id: number) => void;
+  toggleArchiveNote: (id: number) => void;
+};
