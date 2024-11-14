97b42de4868a2e703acf3a7ed1103c8b75eab040
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-14 13:22:12 +0700

    Memberi class pada paragraph Note List

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -3,11 +3,11 @@ import { NoteListProps } from "../utils/types";
 
 const NoteList = ({ notes, deleteNote, toggleArchiveNote }: NoteListProps) => {
   if (notes.length === 0) {
-    return <p>No notes available</p>;
+    return <p className="text-slate-500">No notes available</p>;
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {notes.map((note) => (
         <NoteItem
           key={note.id}
