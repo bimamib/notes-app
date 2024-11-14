7b0d66076b3c0b3c4e36edf5212cf959a8f09bc4
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-14 13:26:46 +0700

    Ubah posisi search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,23 +12,50 @@ const Navbar = ({ onSearch }: NavbarProps) => {
     onSearch(searchTerm);
   };
 
+  // const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  //   if (e.key === "Enter") {
+  //     handleSearchClick();
+  //   }
+  // };
+
   return (
-    <header className="bg-slate-50 p-4 flex justify-between items-center">
+    <header className="flex items-end justify-between p-4 bg-slate-50">
       <h1 className="text-2xl font-bold lg:text-4xl">Notes</h1>
-      <input
-        className="navbar-search-input p-2 border border-gray-200 focus:outline-none focus:outline-blue-500 rounded-lg w-3/5 mr-2 text-sm"
-        type="text"
-        placeholder="Search notes..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      <button
-        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-        type="button"
-        onClick={handleSearchClick}
-      >
-        Search
-      </button>
+      <div className="flex items-center gap-2">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-3.5 z-20">
+            <svg
+              className="w-4 h-4 text-gray-400 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </svg>
+          </div>
+          <input
+            className="py-2 pl-10 pr-4 text-sm border border-gray-200 rounded-lg navbar-search-input focus:outline-none focus:outline-blue-500"
+            type="text"
+            placeholder="Search notes..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <button
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm gap-x-2 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+          type="button"
+          onClick={handleSearchClick}
+        >
+          Search
+        </button>
+      </div>
     </header>
   );
 };
