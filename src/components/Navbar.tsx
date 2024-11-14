aae432dbc422f425412aea046c253cdf8485f1ff
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-14 22:23:56 +0700

    Feat: menambahkan responsif pada navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,10 +19,12 @@ const Navbar = ({ onSearch }: NavbarProps) => {
   };
 
   return (
-    <header className="flex items-end justify-between p-4 bg-slate-50">
-      <h1 className="text-2xl font-bold lg:text-4xl">Notes</h1>
-      <div className="flex items-center gap-2">
-        <div className="relative">
+    <header className="flex flex-col items-center justify-between p-4 bg-slate-50 md:flex-row md:items-end md:justify-between">
+      <h1 className="mb-2 text-2xl font-bold text-center lg:text-4xl md:text-left md:mb-0">
+        Notes
+      </h1>
+      <div className="flex flex-col w-full gap-2 md:flex-row md:items-center md:gap-2 md:w-auto">
+        <div className="relative w-full md:w-64">
           <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-3.5 z-20">
             <svg
               className="w-4 h-4 text-gray-400 shrink-0"
@@ -41,7 +43,7 @@ const Navbar = ({ onSearch }: NavbarProps) => {
             </svg>
           </div>
           <input
-            className="py-2 pl-10 pr-4 text-sm border border-gray-200 rounded-lg navbar-search-input focus:outline-none focus:outline-blue-500"
+            className="w-full py-2 pl-10 text-sm border border-gray-200 rounded-lg focus:outline-none focus:outline-blue-500"
             type="text"
             placeholder="Search notes..."
             value={searchTerm}
@@ -50,7 +52,7 @@ const Navbar = ({ onSearch }: NavbarProps) => {
           />
         </div>
         <button
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm gap-x-2 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+          className="w-full px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 md:w-auto"
           type="button"
           onClick={handleSearchClick}
         >
